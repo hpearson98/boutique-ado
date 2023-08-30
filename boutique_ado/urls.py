430879294659581e40ff32210c2a6ece6a7fbb96
hpearson98 <harry.m.pearson@gmail.com>
2023-08-30 18:33:23 +0000

    Finished the setup for django-allauth

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accouts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
